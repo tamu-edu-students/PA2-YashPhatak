90593a33beff6682608393ae521b57cdf2340ca4
YashPhatak <yashsameerphatak@gmail.com>
2024-09-11 20:35:51 -0500

    changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,25 @@
 #   end
 # Seed the RottenPotatoes DB with some movies.
 more_movies = [
-  {:title => 'My Neighbor Totoro', :rating => 'G',
-    :release_date => '16-Apr-1988'},
-  {:title => 'Green Book', :rating => 'PG-13',
-    :release_date => '16-Nov-2018'},
-  {:title => 'Parasite', :rating => 'R',
-    :release_date => '30-May-2019'},
-  {:title => 'Nomadland', :rating => 'R',
-    :release_date => '19-Feb-2021'},
-  {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+  # {:title => 'My Neighbor Totoro', :rating => 'G',
+  #   :release_date => '16-Apr-1988'},
+  # {:title => 'Green Book', :rating => 'PG-13',
+  #   :release_date => '16-Nov-2018'},
+  # {:title => 'Parasite', :rating => 'R',
+  #   :release_date => '30-May-2019'},
+  # {:title => 'Nomadland', :rating => 'R',
+  #   :release_date => '19-Feb-2021'},
+  # {:title => 'CODA', :rating => 'PG-13',
+  #   :release_date => '13-Aug-2021'},
+  {:title => 'The Terminator', :rating => 'R',
+    :release_date => '26-Oct-1984'},
+  {:title => 'Godzilla', :rating => 'PG-13',
+    :release_date => '16-May-2014'},
+  {:title => 'Insidious', :rating => 'PG-13',
+    :release_date => '01-Apr-2011'},
+  {:title => 'Inception', :rating => 'PG-13',
+    :release_date => '13-July-2010'},
+
 ]
 
 more_movies.each do |movie|
